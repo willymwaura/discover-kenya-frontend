5b62d16617d7f8fb506620eeaa3ec89070a17194
willymwaura <wilsonmwaura697@gmail.com>
2022-06-09 01:51:50 +0900

    urls

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const MovieCard = ({site: { id, region, image_url, title } }) => {
+const MovieCard = ({site: { id, region, image_url,nearby_town, title } }) => {
   return (
     <div className="movie" key={id}>
       <div>
-        <p>{region}</p>
+        <p>{nearby_town}</p>
       </div>
 
       <div>
-        <img src={image_url !== "//cdn.weatherapi.com/weather/64x64/day/116.png" ? image_url : "https://via.placeholder.com/400"} alt={title} />
+        <img src={image_url} alt={title} />
       </div>
 
       <div>
